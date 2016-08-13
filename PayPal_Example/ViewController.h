7cbfde89b2483d0ce9686e7d54f255c3c6c7b95c
ivan4j <progre4j@gmail.com>
2016-08-13 09:50:17 -0500

    Initial Commit

---
+++ PayPal_Example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PayPal_Example
+//
+//  Created by Ivan Hernandez on 13/08/16.
+//  Copyright © 2016 Ivan Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PayPal_Example/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PayPal_Example
+//
+//  Created by Ivan Hernandez on 13/08/16.
+//  Copyright © 2016 Ivan Hernandez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
